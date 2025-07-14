f25c36e0a1849661cbca5019bc95007825c67e93
Abhishek John Charan <113819602+Abhishekjohn1507@users.noreply.github.com>
2025-07-14 10:27:26 +0530

    loop

--- Day1/Largest.java
+++ Day1/Largest.java
@@ -15,15 +15,26 @@ public class Largest {
         int c= in.nextInt();
         System.out.println("enter the numbers " + a +" " + b + " "+ c + " ");
 
-        int max =a;
+//        int max =a;
+//
+//        if (b>max){
+//            max=b;
+//        }if (c>max) {
+//            max=c;
+//        }
 
-        if (b>max){
-            max=b;
-        }if (c>max) {
-            max=c;
-        }
+//         if you dont want to write long code to find max use the
+//        Math.max(a,b);
+
+//
+//        for three number a,b,c
+
+        int max =Math.max(c,Math.max(a,b));
+
 
         System.out.println("Maximum " + max);
+        System.out.println(" MAximum by using the Mart.max" +  Math.max(c,Math.max(a,b)));
 
     }
 }
+
